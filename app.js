91b72a4cb8ad68e0591f7a2c640b8349d68164c7
Emmanuel Yusufu <emmayusufu@gmail.com>
2017-09-23 23:23:09 +0100

    additions

--- app.js
+++ app.js
@@ -1,38 +1,49 @@
-console.log("Starting Awesome Notes App");
-
+console.log('Starting app');
+// attach fs, lodash, yargs module content to fs, _, yargs variable
+// lodash is for modular utilities, yargs is for parsing cmd line commands.
 const fs = require('fs');
 const _ = require('lodash');
-const notes = require('./notes.js');
 const yargs = require('yargs');
 
+// requiring my own files. Require files using url path, add functions to module.exports.* in concerned file
+const notes = require('./note-functions');
 
-// Apply yargs effect to argv
- const argv = yargs.argv;
+// apply yargs formating to argv
+const argv = yargs.argv;
+
+// grab command line commands with nodejs's process.argv array
+const command = argv._[0];
 
 
-var command = argv._[0];
-
-console.log('Command entered: ', command)
 
 
 
 if (command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
     if (note) {
-            console.log('--')
-            console.log('Title: ' + note.title);
-            console.log('Body: ' + note.body);
-    } else if (note === undefined) {
-        console.log('Note title taken')
+        notes.logNote(note);
+    } else {
+        console.log('Note title taken');
     }
-} else if (command === 'list') {
-    console.log('Listing out notes!');
-    notes.getAll();
-} else if (command === 'read') {
-    console.log('Getting notes set for your reading');
-} else if (command === 'remove') {
-    notes.removeNote(argv.title)
-} else {
-    console.log("Don't recognise this command!");
+}   
+else if (command === 'list') {
+    notes.listAll();
+}   
+else if (command === 'read') {
+    var toRead = notes.readNote(argv.title);
+    notes.logNote(toRead);
+}   
+else if (command === 'remove') {
+    var noteRemoved = notes.removeNote(argv.title);
+    if (noteRemoved) {
+        console.log('Successfully removed');
+    } else {
+        console.log('Nothing removed');
+    }
+}   
+else {
+    console.log('command not recognized');
 }
-    
+
+
+
---
+++ note-functions.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+
+const fetchNotes = () => {
+    try {
+        noteString = fs.readFileSync('notes-file.json');
+        return JSON.parse(noteString);
+    } catch(e) {
+        return [];
+    }
+}
+const saveNotes = (notes) => {
+    fs.writeFileSync('notes-file.json', JSON.stringify(notes));
+}
+const logNote = (notes) => {
+    console.log('Note log');
+    console.log('-------------------');
+    console.log('Title ' + notes.title);
+    console.log('Body ' + notes.body);
+}
+
+// every file when using node has access to module variable
+var addNote = (title, body) => {
+    console.log('adding');
+
+    notes = fetchNotes();
+
+    note = {
+        title: title,
+        body: body
+    }
+
+    
+    var duplicateNotes = notes.filter((note) => {
+        return note.title === title;
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+
+};
+
+var listAll = () => {
+    console.log('listing all notes');
+}
+
+var readNote = (title) => {
+    var notes = fetchNotes();
+    var noteToBeRead = notes.filter((note) => {
+        return note.title === title;
+    });
+    return noteToBeRead[0];
+}
+
+var removeNote = (title) => {
+    console.log('Starting note removal');
+    
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => {
+        return note.title !== title;
+    });
+    saveNotes(filteredNotes);
+    if (notes.length !== filteredNotes.length) {
+        return filteredNotes;
+    }
+}    
+
+module.exports = {
+    addNote: addNote,
+    listAll: listAll,
+    readNote: readNote,
+    removeNote: removeNote,
+    logNote: logNote
+}
